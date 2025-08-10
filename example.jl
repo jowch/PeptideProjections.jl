fceffaa48ca5c62c3baead3f15aec4a55e635f8d
Jonathan Chen <jwhc@ucla.edu>
2025-08-10 14:44:16 -0700

    new image

--- example.jl
+++ example.jl
@@ -1,11 +1,11 @@
 using CairoMakie
 using PeptideProjections
 
-let f = Figure(size = 50 .* (15, 5))
+let f = Figure(size = 50 .* (12, 5))
     axs = Axis[]
 
     for (i, theme) in enumerate([Colorful, ColorfulHydropathy, Hydropathy])
-		ax = Axis(f[1, i], yticks = ([-π/2, 0, π/2], ["-π/2", "0", "π/2"]), title = string(theme))
+		ax = Axis(f[1, i], yticks = ([-π/2, 0, π/2], ["-π/2", "0", "π/2"]), title = string(theme), aspect = DataAspect())
         plotwheel!(ax, "LLGDFFRKSKEKIGKEFKRIVQRIKDFLRNLVPRTES", theme = theme)
         push!(axs, ax)
     end
